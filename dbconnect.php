0e6ef622cea014a8f2ff0e5b586ff8c27ab8fee9
kawamotoseiya <seinnto1226@icloud.com>
2020-10-16 02:48:56 +0900

    deploy11

--- dbconnect.php
+++ dbconnect.php
@@ -1,19 +1,10 @@
 <?php 
-        try{
-            $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-            $db['dbname'] = ltrim($db['path'], '/');
-            $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-            $user = $db['user'];
-            $password = $db['pass'];
-            $options = array(
-              PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-              PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-              PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-            );
-            $dbh = new PDO($dsn,$user,$password,$options);
-            return $dbh;
-        }catch(PDOException $e){
-            echo "エラー:".$e->getMessage();
-        }
-          }
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+    
+        $link = mysqli_connect($server, $username, $password, $db);
 ?>

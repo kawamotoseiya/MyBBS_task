4e8f0634b2fa962e7cf17b99c5739658be420cdc
kawamotoseiya <seinnto1226@icloud.com>
2020-10-24 20:48:17 +0900

    blank4

--- comment_cre.php
+++ comment_cre.php
@@ -17,11 +17,17 @@
     <div class="main_body">
     <?php
         require('dbconnect.php');
+        if($_POST['comment']==''){
+            $error['commetn']='blank';
+            echo "コメントが未入力です。";
+        }
         $bbs_id=$_POST['bbs_id'];
-
+        
+        if (empty($error)){
         $statement = $db->prepare('INSERT INTO comments SET bbs_id=?, comment=?, created_at=NOW()+INTERVAL 9 HOUR');
         $statement->execute(array($_POST['bbs_id'], $_POST['comment']));
         echo 'コメントしました';
+        }
         ?>
         <a href="show.php?id=<?php print($bbs_id) ?>">戻る</a>
     </div>

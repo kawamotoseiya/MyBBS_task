4686311cb0b9488dc53570c20591722494e4a898
kawamotoseiya <seinnto1226@icloud.com>
2020-10-25 00:22:21 +0900

    css18

--- show.php
+++ show.php
@@ -27,18 +27,14 @@
             $bbs = $bbses->fetch();
 
         ?>
-        <table>
-            <tr>
-                <td><h3 class="show_username">投稿者：<?php print($bbs['username']); ?></h3></td>
-                <td><h3>作成日：<?php print($bbs['created_at']); ?></h3></td>
-                <td class="show_body"><h3><?php print($bbs['body']); ?></h3></td>
-                <td>
-                    <a href="edit.php?id=<?php print($bbs['id']); ?>">編集</a>
-                    |
-                    <a href="destroy.php?id=<?php print($bbs['id']); ?>">削除</a>
-                </td>
-            </tr>
-        </table>
+        <section class="show_text">
+            <h3 class="show_username">投稿者：<?php print($bbs['username']); ?></h3>
+            <h3>作成日：<?php print($bbs['created_at']); ?></h3>
+            <h3 class="show_body"><?php print($bbs['body']); ?></h3>
+            <a href="edit.php?id=<?php print($bbs['id']); ?>">編集</a>
+            |
+            <a href="destroy.php?id=<?php print($bbs['id']); ?>">削除</a>
+        </section>
         <?php
             $comments = $db->prepare('SELECT * FROM comments WHERE bbs_id=?');
             $comments->execute(array($id));

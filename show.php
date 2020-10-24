22b5a6f1d422eab4196796bfca918d2a90449677
kawamotoseiya <seinnto1226@icloud.com>
2020-10-25 00:31:46 +0900

    css20

--- show.php
+++ show.php
@@ -27,14 +27,14 @@
             $bbs = $bbses->fetch();
 
         ?>
-        <section class="show_text">
+        <div style="width: 100%;word-break:break-all;">
             <h3 class="show_username">投稿者：<?php print($bbs['username']); ?></h3>
             <h3>作成日：<?php print($bbs['created_at']); ?></h3>
-            <h3 class="show_body"><?php print($bbs['body']); ?></h3>
+            <h3><?php print($bbs['body']); ?></h3>
             <a href="edit.php?id=<?php print($bbs['id']); ?>">編集</a>
             |
             <a href="destroy.php?id=<?php print($bbs['id']); ?>">削除</a>
-        </section>
+        </div>
         <?php
             $comments = $db->prepare('SELECT * FROM comments WHERE bbs_id=?');
             $comments->execute(array($id));
